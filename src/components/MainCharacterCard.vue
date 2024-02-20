352354f25e9dfda35464850e54f18fbb4d6a6bd1
fourp92 <panos9229@gmail.com>
2024-02-20 23:11:27 +0200

    feat(MainCharacterCard): init

---
+++ src/components/MainCharacterCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>main card</div>
+</template>
+
+<script setup lang="ts">
+import { markRaw } from 'vue';
+
+defineProps<{
+  //
+}>();
+</script>
+
+<style scoped>
+/*  */
+</style>
