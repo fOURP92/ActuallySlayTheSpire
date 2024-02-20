f3012dbee5c8357b8f63616348afd3f16824eda3
fourp92 <panos9229@gmail.com>
2024-02-20 23:12:18 +0200

    feat(IndexPage): get runs and export to node

---
+++ src/backend/runs.ts
@@ -0,0 +1,5 @@
+function parseRuns(allFiles: FileList) {
+  console.log('THIS IS IN NODE!', allFiles);
+}
+
+export { parseRuns };
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,8 +1,20 @@
-<template>{{ winrate }}</template>
+<template>
+  {{ winrate }}
+
+  <q-btn
+    label="load data"
+    color="primary"
+    class="absolute-center"
+    @click="choseRunsDirectory"
+  />
+
+  <input type="file" style="display: none" class="openFileExplorer" />
+</template>
 
 <script setup lang="ts">
 import axios from 'axios';
 import { ref, watch } from 'vue';
+import { parseRuns } from '../backend/runs';
 
 const winrate = ref('test');
 
@@ -20,4 +32,32 @@ watch(
 );
 
 getPercentage();
+
+function choseRunsDirectory() {
+  //
+  let openExplorer = document.createElement('input');
+  openExplorer.style.display = 'none';
+  openExplorer.classList.add('file-explorer');
+  openExplorer.type = 'file';
+  openExplorer.setAttribute('webkitdirectory', ''); // Allows directory selection
+  openExplorer.setAttribute('directory', ''); // Allows directory selection in newer browsers
+  openExplorer.addEventListener('change', handleDirectorySelection, false);
+  document.body.appendChild(openExplorer);
+  openExplorer.click();
+}
+
+const selectedFile = ref<FileList | null>(null);
+
+function handleDirectorySelection(event: Event) {
+  selectedFile.value = event.target.files;
+}
+
+watch(
+  () => selectedFile.value,
+  () => {
+    if (!selectedFile.value) return;
+    // console.log('selected file is now:', selectedFile.value);
+    parseRuns(selectedFile.value);
+  }
+);
 </script>
