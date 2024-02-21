37827004df40bd761f4f3c1bbe09ca2765e62710
fourp92 <panos9229@gmail.com>
2024-02-22 01:17:03 +0200

    feat(IndexPage): import MainCharacterCard and remove useless code

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,41 +1,24 @@
 <template>
-  <div v-for="(character, index) in results" :key="index">
-    name: {{ character.name }} wins: {{ character.wins }} loses:
-    {{ character.loses }}
-  </div>
-
-  <q-btn
-    label="load data"
-    color="primary"
-    class="absolute-center"
-    @click="choseRunsDirectory"
-  />
-
-  <input type="file" style="display: none" class="openFileExplorer" />
+  <q-page-container>
+    <div class="row">
+      <q-btn label="load data" color="primary" @click="choseRunsDirectory" />
+    </div>
+    <div class="row" style="justify-content: center">
+      <main-character-card
+        v-for="(character, index) in results"
+        :key="index"
+        :character="character"
+        class="q-mr-md q-mb-md"
+      />
+    </div>
+  </q-page-container>
 </template>
 
 <script setup lang="ts">
-import axios from 'axios';
 import { ref, watch } from 'vue';
 import { parseRuns } from '../backend/runs';
 import { CharacterWinRateDto } from '../dtos/CharacterWinRateDto';
-
-const winrate = ref('test');
-
-async function getPercentage() {
-  const res = await axios.get('http://localhost:3000/winrate');
-  console.log('DONE!', res.data);
-  winrate.value = res.data;
-}
-
-watch(
-  () => winrate.value,
-  () => {
-    console.log('winrate:', winrate.value);
-  }
-);
-
-getPercentage();
+import MainCharacterCard from 'src/components/MainCharacterCard.vue';
 
 function choseRunsDirectory() {
   //
@@ -67,4 +50,3 @@ watch(
   }
 );
 </script>
-../dtos/CharacterWinRateDto
