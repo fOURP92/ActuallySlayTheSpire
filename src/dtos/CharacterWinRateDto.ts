c26415db4f338d5dc41db065962891739ee519d1
fourp92 <panos9229@gmail.com>
2024-02-24 19:16:31 +0200

    feat(CharacterWinRateDto): highestAscensionWon property

--- src/dtos/CharacterWinRateDto.ts
+++ src/dtos/CharacterWinRateDto.ts
@@ -2,6 +2,7 @@ export interface CharacterWinRateDto {
   name: 'IRONCLAD' | 'THE_SILENT' | 'DEFECT' | 'WATCHER';
   wins: number;
   loses: number;
+  highestAscensionWon?: string;
 }
 //wip
 //ascension prop
