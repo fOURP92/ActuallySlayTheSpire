64fec9aa7b42a136becfa867fdfb04fbfbf86106
fourp92 <panos9229@gmail.com>
2024-02-25 20:14:43 +0200

    feat(MainCharacterPage): init

---
+++ src/pages/MainCharacterPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>{{ character.name }}</div>
+  <div>{{ character.wins }}</div>
+  <div>{{ character.loses }}</div>
+  <div>{{ character.highestAscensionWon }}</div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { useFilesStore } from '../stores/slayStore';
+import { CharacterWinRateDto } from '@/dtos/CharacterWinRateDto';
+
+const router = useRouter();
+const store = useFilesStore();
+//maybe show run history? like last ten..
+//add notify for when data not loaded for example..
+
+const routeCharacter = router.currentRoute.value.params.character.toUpperCase();
+const character: CharacterWinRateDto = store.results.filter(
+  (char: CharacterWinRateDto) => char.name === routeCharacter
+)[0];
+</script>
+../stores/slayStore
