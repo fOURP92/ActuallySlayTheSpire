b3e76d0f8e6a057bf7e436152e791f892d7e4efd
fourp92 <panos9229@gmail.com>
2024-02-25 20:14:03 +0200

    feat(slayStore): init pinia store

---
+++ src/stores/slayStore.ts
@@ -0,0 +1,8 @@
+import { CharacterWinRateDto } from '@/dtos/CharacterWinRateDto';
+import { defineStore } from 'pinia';
+
+export const useFilesStore = defineStore('files', {
+  state: () => ({
+    results: [] as CharacterWinRateDto[],
+  }),
+});
