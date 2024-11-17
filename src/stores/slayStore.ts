cd89750fddfd3899d20bed7bb0a2a43fa6b3964c
fourp92 <panos9229@gmail.com>
2024-11-18 01:27:02 +0200

    feat(slayStore): Initliaze values

--- src/stores/slayStore.ts
+++ src/stores/slayStore.ts
@@ -5,5 +5,10 @@ export const useFilesStore = defineStore('files', {
   state: () => ({
     results: [] as CharacterWinRateDto[],
     files: [] as FileList[],
+    //fix wrong type..
+    ironcladRuns: [] as FileList[],
+    silentRuns: [] as FileList[],
+    defectRuns: [] as FileList[],
+    watcherRuns: [] as FileList[],
   }),
 });
