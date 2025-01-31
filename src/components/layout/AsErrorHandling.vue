b008693f6cd20a9ffd064abd63206f86380a6d51
fourp92 <panos9229@gmail.com>
2025-01-31 20:01:34 +0200

    feat: AsErrorHandling

---
+++ src/components/layout/AsErrorHandling.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-card v-if="error">
+    <q-card-section> Error Title </q-card-section>
+    <q-card-section> Error Text </q-card-section>
+    <q-card-actions>
+      <q-btn v-close-popup> Ok </q-btn>
+    </q-card-actions>
+  </q-card>
+</template>
+<script setup lang="ts">
+import { AxiosError } from 'axios';
+import { useErrorStore } from '../../stores/errorStore';
+import { computed } from 'vue';
+
+const errorStore = useErrorStore();
+
+const error = computed<AxiosError | null>(() => {
+  return errorStore.currentError;
+});
+</script>
