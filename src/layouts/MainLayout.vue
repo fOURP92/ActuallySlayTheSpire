0f77baa5f94ec98869040edf421d0803ebf4b7cc
fourp92 <panos9229@gmail.com>
2025-01-31 20:01:00 +0200

    refactor: separate component for header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated class="header-style">
-      <q-toolbar>
-        <q-toolbar-title>
-          <div class="text-class">.ActuallySlayTheSpire.</div>
-        </q-toolbar-title>
-      </q-toolbar>
-    </q-header>
+    <as-header />
+
+    <as-error-handling />
 
     <q-page-container>
       <router-view />
@@ -14,16 +10,7 @@
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
-
-<style lang="css" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Autour+One&display=swap');
-.header-style {
-  background-color: rgb(29, 27, 27);
-}
-.text-class {
-  font-family: 'Autour One', system-ui;
-  font-weight: 400;
-  font-style: normal;
-}
-</style>
+<script setup lang="ts">
+import AsHeader from '../components/layout/AsHeader.vue';
+import AsErrorHandling from '../components/layout/AsErrorHandling.vue';
+</script>
