25693e1129777a41bb26e856ca3bb5ddd098dfcb
fourp92 <panos9229@gmail.com>
2025-02-06 00:43:47 +0200

    refactor: swap from camelCase to snake_case to match data obj

--- src/dtos/EventChoiceDto.ts
+++ src/dtos/EventChoiceDto.ts
@@ -1,12 +1,12 @@
 export interface EventChoice {
-  cardsUpgraded: Array<string>;
-  damageHealed: number;
-  damageTaken: number;
-  eventName: string;
+  cards_upgraded: Array<string>;
+  damage_healed: number;
+  damage_taken: number;
+  event_name: string;
   floor: number;
-  goldGain: number;
-  goldLoss: number;
-  maxHpGain: number;
-  maxHpLoss: number;
-  playerChoice: string;
+  gold_gain: number;
+  gold_loss: number;
+  max_hp_gain: number;
+  max_hp_loss: number;
+  player_choice: string;
 }
--- src/dtos/NeowBonusDto.ts
+++ src/dtos/NeowBonusDto.ts
@@ -1,12 +1,12 @@
 export interface NeowBonus {
-  cardsObtained: Array<string>;
-  cardsRemoved: Array<string>;
-  cardsTransformed: Array<string>;
-  cardsUpgraded: Array<string>;
-  damageTaken: number;
-  goldGained: number;
-  goldLost: number;
-  maxHpGained: number;
-  maxHpLost: number;
-  relicsObtained: Array<string>;
+  cards_obtained: Array<string>;
+  cards_removed: Array<string>;
+  cards_transformed: Array<string>;
+  cards_upgraded: Array<string>;
+  damage_taken: number;
+  gold_gained: number;
+  gold_lost: number;
+  maxHp_gained: number;
+  maxHp_lost: number;
+  relics_obtained: Array<string>;
 }
