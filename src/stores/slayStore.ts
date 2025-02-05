7e25b4d26f24448b72bfcccb5a8b79142e1d27b2
fourp92 <panos9229@gmail.com>
2025-02-05 20:05:44 +0200

    fix(slayStore): import correct types

--- src/stores/slayStore.ts
+++ src/stores/slayStore.ts
@@ -1,14 +1,14 @@
 import { CharacterWinRateDto } from '@/dtos/CharacterWinRateDto';
 import { defineStore } from 'pinia';
+import RunDto from '@/dtos/RunDto';
 
 export const useFilesStore = defineStore('files', {
   state: () => ({
     results: [] as CharacterWinRateDto[],
-    files: [] as FileList[],
-    //fix wrong type..
-    ironcladRuns: [] as FileList[],
-    silentRuns: [] as FileList[],
-    defectRuns: [] as FileList[],
-    watcherRuns: [] as FileList[],
+    files: [] as RunDto[],
+    ironcladRuns: [] as RunDto[],
+    silentRuns: [] as RunDto[],
+    defectRuns: [] as RunDto[],
+    watcherRuns: [] as RunDto[],
   }),
 });
