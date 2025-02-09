e398fea9ae3f26b67cc0657297a60bb7721038c2
fourp92 <panos9229@gmail.com>
2025-02-09 20:22:36 +0200

    feat(AsPopup): add customizable popup component with slots for title, content, and actions

---
+++ src/components/AsPopup.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-dialog v-model="alert" persistent>
+    <q-card :style="{ height: props.height, width: props.width }">
+      <q-card-section>
+        <slot name="title"> Title slot </slot>
+      </q-card-section>
+      <q-card-section style="max-height: 500px; overflow: auto">
+        <slot> Default slot </slot>
+      </q-card-section>
+      <q-card-actions>
+        <slot name="actions"> Actions slot </slot>
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+interface AsPopupProps {
+  height?: string | number;
+  width?: string | number;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const props = withDefaults(defineProps<AsPopupProps>(), {
+  height: 'auto',
+  width: '1200px',
+});
+
+const alert = ref(true);
+</script>
