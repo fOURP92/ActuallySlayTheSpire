4249cce4f8ca00ed922e0e4181d0f2bb2eaf95b1
fourp92 <panos9229@gmail.com>
2025-02-14 01:33:46 +0200

    feat(AsRunsTable): deck display

--- src/components/AsRunsTable.vue
+++ src/components/AsRunsTable.vue
@@ -1,4 +1,34 @@
 <template>
+  <as-popup v-if="deck.length > 0">
+    <template #title>
+      <span class="text-h6"> Deck </span>
+    </template>
+
+    <div class="row">
+      <q-list>
+        <q-item v-for="(card, index) in deck" :key="index">
+          <q-item-section style="max-width: 100px">
+            <q-img :src="getImg(card)" width="100px" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-h6">{{ card }}</q-item-label>
+            <q-item-label caption
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+    <template #actions>
+      <div style="display: flex; justify-content: center; width: 100%">
+        <q-btn label="Close" color="primary" @click="deck = []" />
+      </div>
+    </template>
+  </as-popup>
+
   <q-table
     :rows="runs"
     :columns="columns"
@@ -64,6 +94,18 @@
     <template #body-cell-campfires="props">
       <q-td :props="props"> {{ props.row.campfire_choices.length }} </q-td>
     </template>
+
+    <template #body-cell-deck="props">
+      <q-td :props="props">
+        <q-btn
+          label="Show Deck"
+          size="sm"
+          no-caps
+          color="green-4"
+          @click="deck = props.row.master_deck"
+        />
+      </q-td>
+    </template>
   </q-table>
 </template>
 
@@ -73,6 +115,9 @@ import RunDto from '@/dtos/RunDto';
 import timeConverter from '@/services/timestampParse';
 import { QTableColumn } from 'quasar';
 import { markRaw, ref, watch } from 'vue';
+import AsPopup from './AsPopup.vue';
+import cards from '@/services/cardTranslator';
+import { useRouter } from 'vue-router';
 
 markRaw({
   icons,
@@ -110,7 +155,42 @@ const pagination = {
   rowsPerPage: 0,
 };
 
+const router = useRouter();
+const character = router.currentRoute.value.params.character as string;
+
 const tableDense = ref(true);
+
+const deck = ref<Array<string>>([]);
+
+function getImg(card: string) {
+  const path =
+    character === 'ironclad'
+      ? 'red'
+      : character === 'defect'
+      ? 'blue'
+      : character === 'the_silent'
+      ? 'green'
+      : 'purple';
+
+  let url;
+  if (cards[path][card]) {
+    url = `/cards/${path}/${cards[path][card]}`;
+  } else if (cards.curses[card]) {
+    url = `/cards/curse/${cards.curses[card]}`;
+  } else if (cards.status[card]) {
+    url = `/cards/status/${cards.status[card]}`;
+  } else {
+    url = `/cards/colorless/${card}.png`;
+  }
+  return url;
+}
+
+watch(
+  () => deck.value,
+  () => {
+    console.log(deck.value);
+  }
+);
 </script>
 
 <style lang="sass">
